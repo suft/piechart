f99a93a4e7bc8ddcb2e1f7b0e40a486f7ae1aa99
Sufien Tout <sufientout@gmail.com>
2021-08-24 14:53:09 -0400

    Add xstyled configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import {
+  defaultTheme,
+  ThemeProvider,
+  Preflight,
+} from '@xstyled/styled-components'
+
+const theme = {
+  ...defaultTheme,
+}
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Preflight />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
   document.getElementById('root')
 )
