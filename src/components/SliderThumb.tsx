89cb256a89b1a210fddc1d72eed6eb37c7d78dc4
Sufien Tout <sufientout@gmail.com>
2021-08-25 20:37:44 -0400

    Add internal slider components

---
+++ src/components/SliderThumb.tsx
@@ -0,0 +1,42 @@
+import { x } from '@xstyled/styled-components'
+import { IThumbProps } from 'react-range/lib/types'
+
+type SliderThumbProps = {
+  props: IThumbProps
+  isDragged: boolean
+}
+
+const SliderThumb =
+  (value: number, accent: string) =>
+  ({ props, isDragged }: SliderThumbProps): JSX.Element => {
+    return (
+      <x.div
+        {...props}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        w={6}
+        h={6}
+        boxShadow="lg"
+        borderWidth
+        borderRadius
+        borderColor="gray-600"
+        bg="white"
+      >
+        <x.div
+          position="absolute"
+          top={-25}
+          p={0.5}
+          color="white"
+          bg="gray-400"
+          fontWeight="bold"
+          borderRadius
+        >
+          {value}
+        </x.div>
+        <x.div w={2} h={4} bg={isDragged ? accent : 'gray-200'}></x.div>
+      </x.div>
+    )
+  }
+
+export default SliderThumb
---
+++ src/components/SliderTrack.tsx
@@ -0,0 +1,26 @@
+import { x } from '@xstyled/styled-components'
+import { ITrackProps } from 'react-range/lib/types'
+
+type SliderTrackProps = {
+  props: ITrackProps
+  children: React.ReactNode
+}
+
+const SliderTrack =
+  (color: string) =>
+  ({ props, children }: SliderTrackProps): JSX.Element => {
+    return (
+      <x.div
+        {...props}
+        display="flex"
+        w="sm"
+        h={2}
+        borderRadius="full"
+        bg={color}
+      >
+        {children}
+      </x.div>
+    )
+  }
+
+export default SliderTrack
